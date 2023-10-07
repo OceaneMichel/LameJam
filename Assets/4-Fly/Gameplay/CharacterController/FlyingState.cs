672d2449bc7838bb2827826befa8d4eb6c98fa33
Océane <oceane.michel.poly@gmail.com>
2023-10-07 09:47:16 +0200

    4-Fly: Add first TPS with flying ability

---
+++ Assets/4-Fly/Gameplay/CharacterController/FlyingState.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using StarterAssets;
+using UnityEngine;
+
+public class FlyingState : StateMachineBehaviour
+{
+    [SerializeField] private float m_minFlyingHeight;
+    [SerializeField] private float m_flyingGravity;
+    [SerializeField] private float m_movingSpeed;
+    
+    private GameObject wings;
+    private Rigidbody rigidbody;
+    private ThirdPersonController tpsController;
+    private float previousGravity;
+    private float previousMoveSpeed;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.TryGetComponent(out Rigidbody rb))
+            rigidbody = rb;
+
+        if (animator.TryGetComponent(out ThirdPersonController tps))
+        {
+            tpsController = tps;
+            previousGravity = tps.Gravity;
+            previousMoveSpeed = tps.MoveSpeed;
+            
+            tpsController.Gravity = m_flyingGravity;
+            tpsController.MoveSpeed = m_movingSpeed;
+        }
+
+        if (Physics.Raycast(animator.transform.position, -Vector3.up, out RaycastHit hit))
+        {
+            if (Vector3.Distance(hit.point, animator.transform.position) > m_minFlyingHeight)
+            {
+                var wings = animator.GetComponentInChildren<Wings>(true);
+                this.wings = wings.gameObject;
+                
+                // Open wings
+                this.wings.SetActive(true);
+            }
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Add upward force
+        if (!tpsController)
+        {
+            return;
+        }
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Close wings
+        wings.SetActive(false);
+        if (animator.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        {
+            rb.angularVelocity = Vector3.zero;
+            rb.velocity = Vector3.zero;
+        }
+
+        if (tpsController)
+        {
+            tpsController.Gravity = previousGravity;
+            tpsController.MoveSpeed = previousMoveSpeed;
+        }
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
